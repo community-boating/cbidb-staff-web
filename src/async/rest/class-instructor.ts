7a9be38da3a306edeb277a59b9f8498bb1cb6dcc
Jonathan Cole <coleji@bc.edu>
2021-08-18 21:40:23 -0400

    add class instructor endpoint

---
+++ src/async/rest/class-instructor.ts
@@ -0,0 +1,19 @@
+import * as t from 'io-ts';
+import APIWrapper from '../../core/APIWrapper';
+import { HttpMethod } from "../../core/HttpMethod";
+
+export const classInstructorValidator = t.type({
+	INSTRUCTOR_ID: t.number,
+	NAME_FIRST: t.string,
+	NAME_LAST: t.string,
+})
+
+export const validator = t.array(classInstructorValidator)
+
+const path = "/rest/class-instructor"
+
+export const apiw = new APIWrapper({
+	path,
+	type: HttpMethod.GET,
+	resultValidator: validator,
+})
