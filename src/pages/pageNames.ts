075dd1322cadcdfa9d20ae3a014f64e44532cada
Neil Sparks <neil.sparks.95@gmail.com>
2021-09-17 15:03:28 -0600

    chore: unformat for clarity

--- src/pages/pageNames.ts
+++ src/pages/pageNames.ts
@@ -1,10 +1,10 @@
 export enum PageName {
-    HOME = "home",
-    JP_CLASSES = "jpClasses",
-    USERS = "users",
-    USERS_EDIT = "users_edit",
-    USERS_NEW = "users_new",
-    STAGGERED_ORDER = "staggered_order",
-    MANAGE_INSTRUCTORS = "manage_instructors",
-    MANAGE_TAGS = "manage_tags",
+	HOME="home",
+	JP_CLASSES="jpClasses",
+	USERS="users",
+	USERS_EDIT="users_edit",
+	USERS_NEW="users_new",
+	STAGGERED_ORDER="staggered_order",
+	MANAGE_INSTRUCTORS="manage_instructors",
+	MANAGE_TAGS = "manage_tags",
 }
