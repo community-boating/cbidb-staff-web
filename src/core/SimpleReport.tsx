7b16975c1fbbbd6ab6e84b4365acc404304904ee
Jonathan Cole <jon@jcole.io>
2022-04-03 17:20:11 -0400

    working html

--- src/core/SimpleReport.tsx
+++ src/core/SimpleReport.tsx
@@ -55,45 +55,68 @@ const SimpleReportComponent = ({columns, data}) => {
 
 	console.log(pageIndex + "  " + pageSize)
 
-	return <Table striped bordered {...getTableProps()}>
-		<thead>
-			{headerGroups.map((headerGroup) => (
-				<tr {...headerGroup.getHeaderGroupProps()}>
-					{headerGroup.headers.map((column: any) => (
-						// Add the sorting props to control sorting. For this example
-						// we can add them into the header props
-						<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-							{column.render("Header")}
-							{/* Add a sort direction indicator */}
-							<span>
-								{column.isSorted ? (
-									column.isSortedDesc ? (
-										<FontAwesomeIcon icon={faSortUp} className="ms-2" />
-									) : (
-										<FontAwesomeIcon icon={faSortDown} className="ms-2" />
-									)
-								) : null}
-							</span>
-						</th>
-					))}
-				</tr>
-			))}
-		</thead>
-		<tbody {...getTableBodyProps()}>
-			{page.map((row, i) => {
-				prepareRow(row);
-				return (
-					<tr {...row.getRowProps()}>
-						{row.cells.map((cell) => {
-							return (
-								<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-							);
-						})}
+	return <>
+		<Table striped bordered {...getTableProps()}>
+			<thead>
+				{headerGroups.map((headerGroup) => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map((column: any) => (
+							// Add the sorting props to control sorting. For this example
+							// we can add them into the header props
+							<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+								{column.render("Header")}
+								{/* Add a sort direction indicator */}
+								<span>
+									{column.isSorted ? (
+										column.isSortedDesc ? (
+											<FontAwesomeIcon icon={faSortUp} className="ms-2" />
+										) : (
+											<FontAwesomeIcon icon={faSortDown} className="ms-2" />
+										)
+									) : null}
+								</span>
+							</th>
+						))}
 					</tr>
-				);
-			})}
-		</tbody>
-	</Table>
+				))}
+			</thead>
+			<tbody {...getTableBodyProps()}>
+				{page.map((row, i) => {
+					prepareRow(row);
+					return (
+						<tr {...row.getRowProps()}>
+							{row.cells.map((cell) => {
+								return (
+									<td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+								);
+							})}
+						</tr>
+					);
+				})}
+			</tbody>
+		</Table>
+		<div className="row react-bootstrap-table-pagination">
+			<div className="col-md-6 col-xs-6 col-sm-6 col-lg-6"><span className="react-bs-table-sizePerPage-dropdown dropdown">
+				<button id="pageDropDown" type="button"
+						className="btn btn-default btn-secondary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">12
+					</button>
+					<ul className="dropdown-menu " role="menu" aria-labelledby="pageDropDown"><a href="#" 
+							role="menuitem" className="dropdown-item" data-page="12">12</a><a href="#"  role="menuitem"
+							className="dropdown-item" data-page="25">25</a><a href="#"  role="menuitem"
+							className="dropdown-item" data-page="50">50</a><a href="#"  role="menuitem"
+							className="dropdown-item" data-page="1000">1000</a></ul>
+				</span></div>
+			<div className="react-bootstrap-table-pagination-list col-md-6 col-xs-6 col-sm-6 col-lg-6">
+				<ul className="pagination react-bootstrap-table-page-btns-ul">
+					<li className="active page-item" title="1"><a href="#" className="page-link">1</a></li>
+					<li className="page-item" title="2"><a href="#" className="page-link">2</a></li>
+					<li className="page-item" title="3"><a href="#" className="page-link">3</a></li>
+					<li className="page-item" title="4"><a href="#" className="page-link">4</a></li>
+					<li className="page-item" title="next page"><a href="#" className="page-link">&gt;</a></li>
+				</ul>
+			</div>
+		</div>
+	</>
 };
 
 export const SimpleReport: <T_Data>(props: SimpleReportRequiredProps<T_Data>) => JSX.Element = props => <SimpleReportComponent
