04dd9cf42ea816894394d51a853c99b0ef4d3c5f
Jonathan Cole <jon@jcole.io>
2022-10-30 14:23:57 -0400

    add sentry init

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import asc from "./app/AppStateContainer";
 import { createBrowserHistory } from 'history';
+import * as Sentry from '@sentry/browser';
 
 require("./array-polyfill")
+
+const sentryKey = (process.env.config as any).sentryDSN;
+if (sentryKey) {
+	Sentry.init({dsn: sentryKey});
+}
 
 export const history = createBrowserHistory()
 
