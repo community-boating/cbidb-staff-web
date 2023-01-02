ebfeb79164dd2c341959c02196c0b9b2ae007bca
Jonathan Cole <jon@jcole.io>
2023-01-02 17:01:27 -0500

    first pass card scan api (missing a few things)

---
+++ src/async/staff/dockhouse/scan-card.ts
@@ -0,0 +1,43 @@
+import * as t from 'io-ts';
+import APIWrapper from 'core/APIWrapper';
+import { HttpMethod } from "core/HttpMethod";
+import { OptionalString } from 'util/OptionalTypeValidators';
+
+const scanCardMembershipValidator = t.type({
+	assignId: t.number,
+	membershipTypeId: t.number,
+	startDate: OptionalString,
+	expirationDate: OptionalString,
+	discountName: OptionalString,
+	isDiscountFrozen: t.boolean,
+	hasGuestPrivs: t.boolean
+})
+
+const scanCardRatingValidator = t.type({
+	assignId: t.number,
+	membershipTypeId: t.number,
+	startDate: OptionalString,
+	expirationDate: OptionalString,
+	discountName: OptionalString,
+	isDiscountFrozen: t.boolean,
+	hasGuestPrivs: t.boolean
+})
+
+export const scanCardValidator = t.type({
+	personId: t.number,
+	cardNumber: t.string,
+	nameFirst: t.string,
+	nameLast: t.string,
+	bannerComment: OptionalString,
+	specialNeeds: OptionalString,
+	activeMemberships: t.array(scanCardMembershipValidator),
+	personsRatings: t.array(scanCardRatingValidator)
+})
+
+const path = "/staff/dockhouse/scan-card"
+
+export const getWrapper = new APIWrapper({
+	path: path,
+	type: HttpMethod.GET,
+	resultValidator: scanCardValidator
+})
